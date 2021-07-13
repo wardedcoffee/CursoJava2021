e4b678e584f5eaa3cb292360643babc990d83d4c
Marcos_Nascimento <65818386+wardedcoffee@users.noreply.github.com>
2021-07-13 17:55:07 -0300

    13 JUL ended

--- CursoJavaMulti/exercicios/src/classe/ProdutoTeste.java
+++ CursoJavaMulti/exercicios/src/classe/ProdutoTeste.java
@@ -4,21 +4,35 @@ public class ProdutoTeste {
 	
 	public static void main(String[] args) {
 		
-		Produto p1 = new Produto();
-		p1.nome = "Notebook";
-		p1.preco = 4356.89;
-		p1.desconto = 0.25;
+		Produto p1 = new Produto("Notebook", 4356.89);
+		//p1.nome = "Notebook";
+		//p1.preco = 4356.89;
+		//p1.desconto = 0.25;
 		
 		var p2 = new Produto();
 		p2.nome = "Caneta Preta";
 		p2.preco = 12.56;
-		p2.desconto = 0.29;
+		//p2.desconto = 0;
+		
+		Produto p3 = new Produto("Calcinha Preta", 1000);
+		
+		
+		Produto.desconto = 0.50;
+		
 		
 		System.out.println(p1.nome);
 		System.out.println(p2.nome);
+		System.out.println(p3.nome);
 		
-		double precoFinal1 = p1.preco * (1 - p1.desconto);
-		double precoFinal2 = p2.preco * (1 - p2.desconto);
+		double precoFinal1 = p1.precoComDesconto();
+		double precoFinal2 = p2.precoComDesconto();
+		double precoFinal3 = p3.precoComDesconto();
+		
+		System.out.println(precoFinal1);
+		System.out.println(precoFinal2);
+		System.out.println(precoFinal3);
+		
+		
 		double mediaCarrinho = (precoFinal1 + precoFinal2) / 2;
 		
 		System.out.printf("Média do carrinho = R$%.2f.", mediaCarrinho);
