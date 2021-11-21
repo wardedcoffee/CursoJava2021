87a9d45826a2032428e1bf49b034e9c4d4cdc91d
marcos <marcozzymandias@gmail.com>
2021-11-20 23:37:21 -0300

    Início do JDBC,  abrindo conexão e criando o banco de dados

---
+++ exercicios/src/jdbc/TesteConexao.java
@@ -0,0 +1,21 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TesteConexao {
+
+	public static void main(String[] args) throws SQLException {
+		
+		final String url = "jdbc:mysql://localhost:3306";
+		final String usuario = "root";
+		final String senha = "1911";
+		
+		Connection conexao = DriverManager
+				.getConnection(url, usuario, senha);
+		System.out.println("Conexão efetuada com sucesso!");
+		conexao.close();
+	}
+
+}
